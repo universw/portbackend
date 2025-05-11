3ff4ff93c709b6af7c8caab737a852d3c3d5bc20
portfolioprojectwx <jmiaakgasxomhprmut@gmail.com>
2025-05-12 02:32:41 +0900

    s

---
+++ src/main/java/com/portfo/backend/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.portfo.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                            "https://portfofia.web.app",
+                            "https://portfove.vercel.app",
+                            "http://localhost:3000",
+                            "http://localhost:5500"
+                        )
+                        .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
